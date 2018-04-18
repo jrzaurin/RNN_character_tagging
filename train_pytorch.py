4be191c5db75eff50bd5cd3741141b32006cb20b
Ubuntu <ubuntu@ip-10-0-0-14.eu-west-1.compute.internal>
2018-04-18 17:18:43 +0000

    some progress

--- train_pytorch.py
+++ train_pytorch.py
@@ -5,10 +5,15 @@ import sys
 from glob import glob
 from random import choice
 import numpy as np
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Dense, Dropout, LSTM, TimeDistributed, Bidirectional
-from keras.models import Sequential, load_model
 from text_utils import char2vec, n_chars
+from tqdm import tqdm,trange
+
+import torch
+import torch.nn  as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
 
 
 def chars_from_files(list_of_files):
@@ -18,7 +23,6 @@ def chars_from_files(list_of_files):
             chars = f.read()
             for c in chars:
                 yield c
-
 
 def splice_texts(files_a, jump_size_a, files_b, jump_size_b):
     a_chars = chars_from_files(files_a)
@@ -38,9 +42,6 @@ def splice_texts(files_a, jump_size_a, files_b, jump_size_b):
         source_ind = 1 - source_ind
 
 
-import torch
-from torch.autograd import Variable
-from torch.utils.data import Dataset, DataLoader
 
 dir_a = 'data/sklearn_clean/'
 dir_b = 'data/scalaz_clean/'
@@ -52,6 +53,14 @@ seq_len = 100
 ngen = 1024
 jump_size_a = [20, 200]
 jump_size_b = [20, 200]
+min_jump_size_a = 20
+max_jump_size_a = 200
+min_jump_size_b = 20
+max_jump_size_b = 200
+batch_size = 1024
+seq_len = 100
+juma = [min_jump_size_a, max_jump_size_a]
+jumb = [min_jump_size_b, max_jump_size_b]
 
 
 class CharRNNLoader(Dataset):
@@ -93,11 +102,261 @@ class CharRNNLoader(Dataset):
         return sys.maxsize
         # return 5
 
+# chardataset = CharRNNLoader(seq_len, char2vec, splice_texts, 1024, train_a, train_b, jump_size_a, jump_size_b)
+# train_loader = DataLoader(dataset=chardataset, batch_size=1, shuffle=False)
 
-chardataset = CharRNNLoader(seq_len, char2vec, splice_texts, 1024, train_a, train_b, jump_size_a, jump_size_b)
-train_loader = DataLoader(dataset=chardataset, batch_size=1, shuffle=False)
+class SequenceWise(nn.Module):
+    def __init__(self, module):
+        """
+        Collapses input of dim T*N*H to (T*N)*H, and applies to a module.
+        Allows handling of variable sequence lengths and minibatch sizes.
+        :param module: Module to apply input to.
+        """
+        super(SequenceWise, self).__init__()
+        self.module = module
 
-#######################
+    def forward(self, x):
+        t, n = x.size(0), x.size(1)
+        x = x.contiguous().view(t * n, -1)
+        x = self.module(x)
+        x = x.contiguous().view(t, n, -1)
+        return x
+
+    def __repr__(self):
+        tmpstr = self.__class__.__name__ + ' (\n'
+        tmpstr += self.module.__repr__()
+        tmpstr += ')'
+        return tmpstr
+
+
+def generate_batches(files_a, jump_size_a, files_b, jump_size_b, batch_size, sample_len, return_text=False):
+    # This generators will generate an infinite sequence of characters
+    # belonging to source 1/0. per bach, the sequence will be "cut" as
+    # sample_len
+    gens = [splice_texts(files_a, jump_size_a, files_b, jump_size_b) for _ in range(batch_size)]
+    while True:
+        X = []
+        y = []
+        texts = []
+        for g in gens:
+            chars = []
+            vecs = []
+            labels = []
+            for _ in range(sample_len):
+                c, l = g.__next__()
+                vecs.append(char2vec[c])
+                labels.append([l])
+                chars.append(c)
+            X.append(vecs)
+            y.append(labels)
+
+            if return_text:
+                texts.append(''.join(chars))
+
+        if return_text:
+            yield (np.array(X), np.array(y), texts)
+        else:
+            yield (np.array(X), np.array(y))
+
+
+# class RNNCharTagger(nn.Module):
+#     def __init__(self):
+#         super(RNNCharTagger, self).__init__()
+
+#         self.lstm1 = nn.LSTM(96, 128, batch_first=True, dropout=0.2)
+#         self.lstm2 = nn.LSTM(128, 128, batch_first=True, dropout=0.2)
+#         self.lstm3 = nn.LSTM(128, 128, batch_first=True, dropout=0.2)
+#         self.linear = SequenceWise(nn.Linear(128, 1))
+
+#         self.h1 = nn.Parameter(torch.zeros(1, 1024, 128))
+#         self.c1 = nn.Parameter(torch.zeros(1, 1024, 128))
+#         self.h2 = nn.Parameter(torch.zeros(1, 1024, 128))
+#         self.c2 = nn.Parameter(torch.zeros(1, 1024, 128))
+#         self.h3 = nn.Parameter(torch.zeros(1, 1024, 128))
+#         self.c3 = nn.Parameter(torch.zeros(1, 1024, 128))
+
+#     def forward(self, X):
+
+#         output1, (h1, c1) = self.lstm1(X,       (self.h1, self.c1))
+#         output2, (h2, c2) = self.lstm2(output1, (self.h2, self.c2))
+#         output3, (h3, c3) = self.lstm3(output2, (self.h3, self.c3))
+
+#         self.h1 = nn.Parameter(h1.data)
+#         self.c1 = nn.Parameter(c1.data)
+#         self.h2 = nn.Parameter(h2.data)
+#         self.c2 = nn.Parameter(c2.data)
+#         self.h3 = nn.Parameter(h3.data)
+#         self.c3 = nn.Parameter(c3.data)
+
+#         output = F.sigmoid(self.linear(output3))
+
+#         return output
+
+
+class RNNCharTagger(nn.Module):
+    def __init__(self, lstm_layers, input_dim, out_dim, batch_size=1024,dropout=0.2, batch_first=True):
+        super(RNNCharTagger, self).__init__()
+
+        self.lstm_layers = lstm_layers
+        self.input_dim = input_dim
+        self.out_dim = out_dim
+        self.dropout = dropout
+        self.batch_first = batch_first
+        self.batch_size = batch_size
+
+        self.lstm1 =  nn.LSTM(self.input_dim, self.out_dim, batch_first=self.batch_first, dropout=self.dropout)
+        for i in range(1,self.lstm_layers):
+            setattr(self, 'lstm'+str(i+1), nn.LSTM(self.out_dim, self.out_dim, batch_first=self.batch_first, dropout=self.dropout))
+        self.linear = SequenceWise(nn.Linear(self.out_dim, 1))
+
+        for i in range(self.lstm_layers):
+            setattr(self, 'h'+str(i+1), nn.Parameter(torch.zeros(1, self.batch_size, self.out_dim)))
+            setattr(self, 'c'+str(i+1), nn.Parameter(torch.zeros(1, self.batch_size, self.out_dim)))
+
+    def forward(self, X):
+
+        output, (h1, c1) = self.lstm1(X, (self.h1, self.c1))
+        hidden_states = [(h1,c1)]
+        for i in range(1,self.lstm_layers):
+            h,c = getattr(self, 'h'+str(i+1)), getattr(self, 'c'+str(i+1))
+            output, (nh,nc) = getattr(self, 'lstm'+str(i+1))(output, (h,c))
+            hidden_states.append((nh,nc))
+
+        for i in range(self.lstm_layers):
+            setattr(self, 'h'+str(i+1), nn.Parameter(hidden_states[i][0].data))
+            setattr(self, 'c'+str(i+1), nn.Parameter(hidden_states[i][1].data))
+
+        output = F.sigmoid(self.linear(output))
+
+        return output
+
+model = RNNCharTagger(3,96,128)
+model = model.cuda()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+train_gen = generate_batches(train_a, juma, train_b, jumb, batch_size, seq_len)
+
+epochs = 2
+steps_per_epoch = 100
+for epoch in range(epochs):
+    with trange(steps_per_epoch) as t:
+        for i in t:
+            t.set_description('epoch %i' % epoch)
+            X,y = train_gen.__next__()
+            inp, targets = Variable(torch.from_numpy(X).float()), Variable(torch.from_numpy(y).float())
+            inp, targets = inp.cuda(), targets.cuda()
+            model.zero_grad()
+            # model.hidden = model.init_hidden()
+            scores = model(inp)
+            loss = criterion(scores, targets)
+            t.set_postfix(loss=loss.data[0])
+            loss.backward()
+            optimizer.step()
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+class Accuracy(nn.Module):
+    def __init__(self):
+        super(Accuracy, self).__init__()
+
+    def forward(self,y_pred,y):
+        y_pred = (y_pred.view(-1, 1) > 0.5).data.float()
+        y = y.view(-1, 1).data.float()
+        acc = (y_pred == y).sum()/y.size(0)
+        return Variable(torch.FloatTensor([acc]))
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(\n)'
+
+model = RNNCharTagger(3,96,128)
+model = model.cuda()
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+train_gen = generate_batches(train_a, juma, train_b, jumb, batch_size, seq_len)
+epochs = 2
+steps_per_epoch = 100
+
+def train(train_gen, model, criterion, optimizer, epoch, steps_per_epoch):
+
+    # switch to train mode
+    model.train()
+
+    with trange(steps_per_epoch) as t:
+        for i in t:
+            t.set_description('epoch %i' % epoch)
+
+            X,y = train_gen.__next__()
+            X_var = Variable(torch.from_numpy(X).float())
+            y_var = Variable(torch.from_numpy(y).float())
+            if use_cuda:
+                X_var, y_var = X_var.cuda(), y_var.cuda()
+
+            optimizer.zero_grad()
+            # model.hidden = model.init_hidden()
+            y_pred = model(X_var)
+            loss = criterion(y_pred, y_var)
+            t.set_postfix(loss=loss.data[0])
+            loss.backward()
+            optimizer.step()
+
+
+for epoch in range(epochs):
+    train(train_gen, model, criterion, optimizer, epoch, steps_per_epoch)
+
+def validate(val_gen, model, metrics, validation_steps):
+
+    # switch to evaluate mode
+    model.eval()
+    if isinstance(metrics, list):
+        losses = []
+        for i in range(len(metrics)):
+            losses.append(AverageMeter())
+
+    with trange(validation_steps) as t:
+        for i in t:
+            X,y = val_gen.__next__()
+            X_var = Variable(torch.from_numpy(X).float())
+            y_var = Variable(torch.from_numpy(y).float())
+            if use_cuda:
+                X_var, y_var = X_var.cuda(), y_var.cuda()
+            y_pred = model(X_var)
+            if isinstance(metrics, list):
+                for i in range(len(metrics)):
+                    losses[i].update(metrics[i](y_pred, y_var).data[0])
+            else:
+                loss = metrics(y_pred,y_var)
+
+    for metric,loss in zip(metrics, losses):
+        print("val_{}: {}".format(metric.__repr__().split("(")[0], loss.val))
+
+
+
+
+
+###############################################################################
+
+
+
+
+
+
 
 
 def generate_batches(files_a, jump_size_a, files_b, jump_size_b, batch_size, sample_len, return_text=False):
@@ -169,57 +428,3 @@ def main(model_path, dir_a, dir_b, min_jump_size_a, max_jump_size_a, min_jump_si
                             validation_steps=100,
                             epochs=epochs,
                             callbacks=[checkpointer])
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser("train tagger and save trained model")
-    parser.add_argument("model_path", help=
-        "Path where to save trained model. If this path exists, a model will be loaded from it. "
-        "Otherwise a new one will be constructed. The model will be saved to this path after "
-        "every epoch.")
-    parser.add_argument("dir_a", help="directory with first source of input files. It should "
-                                      "contain 'train' and 'test' subdirectories that contain "
-                                      "actual files")
-    parser.add_argument("dir_b", help="directory with second source of input files. It should "
-                                      "contain 'train' and 'test' subdirectories that contain "
-                                      "actual files")
-    parser.add_argument("--min_jump_a", type=int, default=20, help="snippets from source A will "
-                                                                   "be at least this long")
-    parser.add_argument("--max_jump_a", type=int, default=200, help="snippets from source B will "
-                                                                    "be at most this long")
-    parser.add_argument("--min_jump_b", type=int, default=20, help="snippets from source B will "
-                                                                   "be at least this long")
-    parser.add_argument("--max_jump_b", type=int, default=200, help="snippets from source B will "
-                                                                    "be at most this long")
-    parser.add_argument("--sequence_length", type=int, default=100, help="how many characters in "
-                                                                         "single sequence")
-    parser.add_argument("--batch_size", type=int, default=1024)
-    parser.add_argument("--rnn_size", type=int, default=128, help="how many LSTM units per layr")
-    parser.add_argument("--lstm_layers", type=int, default=3, help="how many LSTM layers")
-    parser.add_argument("--dropout_rate", type=int, default=0.2, help="dropout rate for a "
-                                                                      "droupout layer inserted "
-                                                                      "after every LSTM layer")
-    parser.add_argument("--bidirectional", action="store_true",
-                        help="Whether to use bidirectional LSTM. If true, inserts a backwards LSTM"
-                        " layer after every normal layer.")
-    parser.add_argument("--steps_per_epoch", type=int, default=1000)
-    parser.add_argument("--epochs", type=int, default=1000)
-
-    args = parser.parse_args()
-
-    main(
-        args.model_path,
-        args.dir_a,
-        args.dir_b,
-        args.min_jump_a,
-        args.max_jump_a,
-        args.min_jump_b,
-        args.max_jump_b,
-        args.sequence_length,
-        args.batch_size,
-        args.rnn_size,
-        args.lstm_layers,
-        args.dropout_rate,
-        args.bidirectional,
-        args.steps_per_epoch,
-        args.epochs)
